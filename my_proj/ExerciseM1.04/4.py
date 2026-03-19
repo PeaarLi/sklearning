575ffe25d4fc355c814412f7780c16551c624464
LiYi <qysxlbq@gmail.om>
2026-03-19 11:28:36 +0800

    Exercise 4

--- my_proj/ExerciseM1.04/4.py
+++ my_proj/ExerciseM1.04/4.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.compose import make_column_selector
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import cross_validate
 from sklearn.linear_model import LogisticRegression
 
@@ -16,10 +17,17 @@ target = data[target_name]
 cat_selecter = make_column_selector(dtype_include="object")
 cat_data = data[cat_selecter(data)].drop(columns=target_name)
 
-model = make_pipeline(
+model1 = make_pipeline(
     OrdinalEncoder(handle_unknown="use_encoded_value",unknown_value=-1),
     LogisticRegression(max_iter=500)
     )
-cv_results = cross_validate(model,cat_data,target)
-
-print_cv(cv_results)
+cv_results1 = cross_validate(model1,cat_data,target)
+model2 = make_pipeline(
+    OneHotEncoder(handle_unknown="ignore"),
+    LogisticRegression(max_iter=500)
+)
+cv_results2 = cross_validate(model2,cat_data,target)
+print("OrigianlEnoder:")
+print_cv(cv_results1) 
+print("OneHotEncoder:")
+print_cv(cv_results2)
